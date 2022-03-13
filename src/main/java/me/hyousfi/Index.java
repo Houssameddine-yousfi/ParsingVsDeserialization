8026ad1f4ec2fad97efa5da75fa553434114ccde
Houssameddine Yousfi <houssameddine.yousfi@ensma.fr>
2022-03-13 17:20:56 +0100

    Compare peformance between parsing ad deserialization

--- src/main/java/me/hyousfi/Index.java
+++ src/main/java/me/hyousfi/Index.java
@@ -6,11 +6,9 @@ import com.dataexplolink.pqdag.Spatial.Entities.Geometry;
 import com.dataexplolink.pqdag.Spatial.Exceptions.WKTParsingException;
 import org.locationtech.jts.geom.GeometryFactory;
 
+import javax.sound.midi.Soundbank;
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 
@@ -117,10 +115,106 @@ public class Index {
 
     public static void main(String args[]){
 
-        //save geo objects as JTSs in sqlite
-            //use JTS parser for this
-            //use my parser also to compare
-        //run a filter using the parsing
-        //urn a filter using the deserialization
+        int i=0,j=0;
+
+        // Connection to database with strings
+        Connection connection2 = null;
+        try {
+            connection2 = createConnection(DB_URL_STRINGS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Initialization of the query and the geo factory
+        String select_all_query2 = "Select wkt from strings";
+        GeoFactory factory = DependencyManager.getGeometryFactory();
+
+        try {
+            // Running the query
+            Statement statement = connection2.createStatement();
+            ResultSet rs = statement.executeQuery(select_all_query2);
+
+            //Fetching the results
+            long start = System.currentTimeMillis();
+            while (rs.next()){
+
+                //Getting the line from the result set
+                String line = rs.getString("wkt");
+
+                //the line contain some prefix seperated with "|" this is why we parse the last part
+                String[] parts = line.split("\\|");
+                Geometry g = factory.parseWKT(parts[4].substring(1));
+
+                //just a check if g is not null to force JVM to parse things
+                if (g != null)
+                    j++;
+            }
+
+            //calculating and printing execution time
+            long end = System.currentTimeMillis();
+            long parsing_time = end - start;
+            System.out.println("Parsing of " + j + " objects in " + parsing_time + " ms");
+
+            //closing the statment and the connection
+            statement.close();
+            connection2.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (WKTParsingException e) {
+            e.printStackTrace();
+        }
+
+
+        // Connection to database with strings
+        Connection connection = null;
+        try {
+            connection = createConnection(DB_URL_BLOBS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Initialization of the query and
+        String select_all_query = "Select s_object from strings";
+
+        try {
+            // Running the query
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(select_all_query);
+
+            //Fetching the results
+            long start = System.currentTimeMillis();
+            while (rs.next()){
+
+                //getting the bytes from the result set
+                byte[] bytes = rs.getBytes("s_object");
+
+                //using the Object inputs stream to deserilize the objects
+                ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+                ObjectInputStream in = new ObjectInputStream(bis);
+                Geometry g = (Geometry) in.readObject();
+                //System.out.println(g);
+
+                //just a check if g is not null to force JVM to serelize things
+                if (g != null)
+                    i++;
+            }
+
+            //calculating and printing execution time
+            long end = System.currentTimeMillis();
+            long sereialization_time = end - start;
+            System.out.println("De-serialisation of " + i + " objects in " + sereialization_time + " ms");
+
+            //closing the statment and the connection
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
